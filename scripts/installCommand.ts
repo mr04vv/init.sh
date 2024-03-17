95171a6e8a325163eeeac203cd4557603a0aff8c
Takuto Mori <mr04vv@gmail.com>
2024-03-17 14:05:08 +0900

    :rocket: feat: create scripts directory

---
+++ scripts/installCommand.ts
@@ -0,0 +1,8 @@
+export const installAppComands: string[] = [
+	"brew install --cask bruno",
+	"brew install --cask google-chrome",
+	"brew install --cask microsoft-edge",
+	"brew install --cask figma",
+	"brew install --cask firefox",
+	"brew install --cask jetbrains-toolbox",
+];
